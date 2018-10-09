244f72fb7773d70626a7daf7000be56229292221
Luke Breen <l.breen1@outlook.com>
2018-10-09 17:59:12 +0300

    Migrating from middleman to rails iot allow for expansion of the project

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+
+  get '/about', to: 'pages#about', as: 'about'
+  get '/contact', to: 'pages#contact', as: 'contact'
+  get '/pricing', to: 'pages#pricing', as: 'pricing'
+  get '/programme', to: 'pages#programme', as: 'programme'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
