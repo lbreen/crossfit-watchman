4edc18821db45a77fc582ffc9e2b8e5397b78661
Luke Breen <l.breen1@outlook.com>
2018-11-09 11:17:07 +0000

    Added simple calender and personalised css

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,39 @@ claire = Instructor.create(first_name: 'Claire', last_name: 'Small', qualificati
 lee = Instructor.create(first_name: 'Lee', last_name: 'Maxwell', qualification: 'Level 1 CrossFit Instructor')
 
 
-Lesson.create(
+Lesson.create!(
   title: "Workout of the Day",
-  start_time: Time.new('2018', '10', '19', '16', '30'),
-  end_time: Time.new('2018', '10', '19', '17', '30'),
+  start_time: Time.new('2018', '11', '10', '16', '30'),
+  end_time: Time.new('2018', '11', '10', '17', '30'),
   instructor: rob,
   description: 'One of our various daily CrossFit sessions'
   )
-
-Lesson.create(
+Lesson.create!(
   title: "Workout of the Day",
-  start_time: Time.new('2018', '10', '23', '12', '30'),
-  end_time: Time.new('2018', '10', '23', '13', '30'),
+  start_time: Time.new('2018', '11', '13', '16', '30'),
+  end_time: Time.new('2018', '11', '13', '17', '30'),
+  instructor: lee,
+  description: 'One of our various daily CrossFit sessions'
+  )
+Lesson.create!(
+  title: "Workout of the Day",
+  start_time: Time.new('2018', '11', '15', '16', '30'),
+  end_time: Time.new('2018', '11', '15', '17', '30'),
+  instructor: rob,
+  description: 'One of our various daily CrossFit sessions'
+  )
+Lesson.create!(
+  title: "Workout of the Day",
+  start_time: Time.new('2018', '11', '17', '16', '30'),
+  end_time: Time.new('2018', '11', '17', '17', '30'),
+  instructor: lee,
+  description: 'One of our various daily CrossFit sessions'
+  )
+
+Lesson.create!(
+  title: "Workout of the Day",
+  start_time: Time.new('2018', '11', '23', '12', '30'),
+  end_time: Time.new('2018', '11', '23', '13', '30'),
   instructor: claire,
   description: 'One of our various daily CrossFit sessions'
   )
